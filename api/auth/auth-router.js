9594c9eb0f0c02c8e1317a644f2c5a2ffd196539
Sebastian <sebas6292@gmail.com>
2021-11-08 17:17:55 -0800

    done

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -87,7 +87,17 @@ checkUsernameFree, (req, res, next) => {
   }
  */
   router.get('/logout', (req, res, next) => {
-    res.json('logout')
+    if (req.session.user) {
+      req.session.destroy(err => {
+        if (err) {
+          next(err)
+        } else {
+          res.json({ message: "logged out"})
+        }
+      })
+    } else {
+      res.json({message: "no session" })
+    }
   })
   
  
